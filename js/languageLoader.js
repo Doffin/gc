47c70f0622fb7fb217357fd466d241c604cce6c2
Dagfinn Opsvik <dagfinn@opsvik.com>
2025-11-17 19:09:51 +0100

    Update

--- js/languageLoader.js
+++ js/languageLoader.js
@@ -27,10 +27,10 @@ function updateText() {
             el.textContent = interpolate(value, currentVariables);
         }
     });
-    document.getElementById("chartTitle").textContent = interpolate(currentLanguageData.content.graphTitle, currentVariables);
-    chart.options.plugins.title.text = interpolate(currentLanguageData.content.graphTitle, currentVariables);
-    chart.options.scales.x.title.text = interpolate(currentLanguageData.content.xAxisTitle, currentVariables);
-    chart.options.scales.y.title.text = interpolate(currentLanguageData.content.yAxisTitle, currentVariables);
+    document.getElementById("chartTitle").textContent = interpolate("graphTitle", currentVariables);
+    chart.options.plugins.title.text = interpolate("graphTitle", currentVariables);
+    chart.options.scales.x.title.text = interpolate("xAxisTitle", currentVariables);
+    chart.options.scales.y.title.text = interpolate("yAxisTitle", currentVariables);
     chart.update();
 }
 
