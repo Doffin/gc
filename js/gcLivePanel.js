92a26c6a8701d14bf81ab99a55be82f8c3f4f480
Dagfinn Opsvik <dagfinn@opsvik.com>
2025-11-19 12:45:17 +0100

    Test

--- js/gcLivePanel.js
+++ js/gcLivePanel.js
@@ -45,9 +45,9 @@ class gcLivePanel extends HTMLElement {
         <div class="title" data-i18n="livesensor.title">Live Sensor Data</div>
         <div id="sensorContainer">
         <table style="width:100%;">
-        <tr><td><label data-i18n="livesensor.tid">Tid</label></td> <td><span id="tid">--</span></td><td><span id="tidUnit">--</span></td></tr>
-        <tr><td><label data-i18n="livesensor.last">Last</label></td> <td><span id="last">--</span></td><td><span id="lastUnit">--</span></td></tr>
-        <tr><td><label data-i18n="livesensor.setning">Setning</label></td> <td><span id="setning">--</span></td><td><span id="setningUnit">--</span></td></tr>
+        <tr><td><label data-i18n="livesensor.tid">Tid</label></td> <td><span id="tid">--</span></td><td><span data-i18n="livesensor.tidUnit">--</span></td></tr>
+        <tr><td><label data-i18n="livesensor.last">Last</label></td> <td><span id="last">--</span></td><td><span data-i18n="livesensor.lastUnit">--</span></td></tr>
+        <tr><td><label data-i18n="livesensor.setning">Setning</label></td> <td><span id="setning">--</span></td><td><span data-i18n="livesensor.setningUnit">--</span></td></tr>
         </table>
         </div>
       </div>
